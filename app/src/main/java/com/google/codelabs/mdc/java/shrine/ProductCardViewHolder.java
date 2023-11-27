7c224896d9281f6bddbf211062718778a96a3ce0
Borja <amanecerbo2@gmail.com>
2023-11-27 13:17:43 +0100

    102-en proceso

--- app/src/main/java/com/google/codelabs/mdc/java/shrine/ProductCardViewHolder.java
+++ app/src/main/java/com/google/codelabs/mdc/java/shrine/ProductCardViewHolder.java
@@ -1,13 +1,24 @@
 package com.google.codelabs.mdc.java.shrine;
 
 import android.view.View;
+import android.widget.TextView;
+
+import com.android.volley.toolbox.NetworkImageView;
+import com.google.codelabs.mdc.java.shinre.R;
+
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 public class ProductCardViewHolder extends RecyclerView.ViewHolder {
 
+    public NetworkImageView productImage;
+    public TextView productTitle;
+    public TextView productPrice;
+
     public ProductCardViewHolder(@NonNull View itemView) {
         super(itemView);
-        //TODO: Find and store views from itemView
+        productImage = itemView.findViewById(R.id.product_image);
+        productTitle = itemView.findViewById(R.id.product_title);
+        productPrice = itemView.findViewById(R.id.product_price);
     }
 }
